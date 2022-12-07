4e1f1d6a1df4b13ad4d31164cc8727ad19fa07fc
Emil Andreas Lund <elu@teleplan.no>
2022-12-07 23:49:05 +0100

    dag 7

--- Des7/Des7.py
+++ Des7/Des7.py
@@ -1,19 +1,14 @@
-import re
 f = open("Des7/input.txt", "r")
-
 lines = f.readlines()
 
 sizes = {}
-
-def getDirLines(dirName, dirPath):
-    print("current search path: ", dirPath)
+def getDirContent(dirName, dirPath):
     currentPath = []
     dirlines = []
     for i in range(len(lines)):
         line = lines[i].strip()
         if(line[:4] == "$ cd"):
             name = line[4:].strip()
-            print("compare: ", dirPath, currentPath, dirPath == currentPath, "name: ", dirName, name)
             if(name == dirName and dirPath == currentPath):
                 j=0
                 while(i+2+j < len(lines) and lines[i+2+j][0] != '$' ):
@@ -24,42 +19,25 @@ def getDirLines(dirName, dirPath):
                 currentPath = currentPath [:-1]
             else:
                 currentPath.append(name)
-            print("currentPath", currentPath)
     return dirlines
 
-na = []
-nb = []
-
-for line in lines:
-    if(line[:4] == "$ cd"):
-        name = line[4:].strip()
-        if(name != ".."):
-            na.append(name)
-
 def getDirScore(dirName, dirPath):
-    if(dirName == "qsgv"):
-        print('a')
-    dirlines = getDirLines(dirName, dirPath)
+    dirlines = getDirContent(dirName, dirPath)
     size = 0
     for line in dirlines:
         if(line[:3].strip() == "dir"):
             n = line[3:].strip()
             relativePath = dirPath + [dirName]
-            fullPath = relativePath + [n]
-            print("fullpath: ", fullPath, "/".join(fullPath))
-            if "/".join(fullPath) in sizes:
-                size += sizes["/".join(fullPath)]
+            fullPathString = "/".join(relativePath + [n])
+            if fullPathString in sizes:
+                size += sizes[fullPathString]
             else: size += getDirScore(n, relativePath)
         else: 
             size += int(line.split(" ")[0])
-    fullPath = dirPath + [dirName]
-    fullPathString = "/".join(fullPath)
+    fullPathString = "/".join(dirPath + [dirName])
     sizes[fullPathString] = size
-    nb.append(dirName)
     return size
 
-# for dirName in set(na):
-#     getDirScore(dirName)
 
 def getResult1_b():
     score = 0
@@ -69,32 +47,14 @@ def getResult1_b():
     return score
 
 def getResult2():
-    maxSize = 70000000
-    currSize = sizes["/"]
-    freeSizeNeeded = 30000000
-    task2Sizes = []
-    for (key, val) in sizes.items():
-        task2Sizes.append(val)
-    task2Sizes.sort()
-    for size in task2Sizes:
-        if(maxSize - currSize + size > freeSizeNeeded):
+    allSizes = list(map(lambda x: x[1], sizes.items()))
+    allSizes.sort()
+    for size in allSizes:
+        if(70000000 - sizes["/"] + size > 30000000):
             return size
 
 
 
-print(getDirScore("/", []))
+getDirScore("/", [])
 print(getResult1_b())
 print(getResult2())
-
-
-# print(na)
-# print(nb)
-
-# print("na: ", len(na), len(set(na)))
-# print("nb: ", len(nb), len(set(nb)))
-# opposite = set(na).difference(set(nb))
-# oppositeb = set(nb).difference(set(na))
-# print(oppositeb)
-# for o in opposite:
-#     print(o)
-# print(sizes)
